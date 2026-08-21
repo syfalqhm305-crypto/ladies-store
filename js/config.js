0a2f2a2793abd5bc9c86fc2e42f1f9c5043023f7
syfalqhm305-crypto <syfalqhm305@gmail.com>
2026-08-21 05:44:44 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,2 +0,0 @@
-window.SUPABASE_URL = "https://zeuxpeqnokjgjxaxwovd.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_TagAb3ZSTUUFqV43KlRY2g_qACPvm7S";
--- js/supabase-client.js
+++
@@ -1,6 +0,0 @@
-const supabaseClient = window.supabase.createClient(
-  window.SUPABASE_URL,
-  window.SUPABASE_ANON_KEY
-);
-
-window.supabaseClient = supabaseClient;
